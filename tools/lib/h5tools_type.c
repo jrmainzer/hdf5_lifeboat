jhendersonHDF/hdf5

Implement support for complex number datatypes
Adds the new datatype class H5T_COMPLEX

Adds the new API function H5Tcomplex_create which creates a complex
number datatype from an ID of a base floating-point datatype

Adds the new feature check macros H5_HAVE_COMPLEX_NUMBERS and
H5_HAVE_C99_COMPLEX_NUMBERS

Adds the new datatype size macros H5_SIZEOF_FLOAT_COMPLEX,
H5_SIZEOF_DOUBLE_COMPLEX and H5_SIZEOF_LONG_DOUBLE_COMPLEX

Adds the new datatype ID macros H5T_NATIVE_FLOAT_COMPLEX,
H5T_NATIVE_DOUBLE_COMPLEX, H5T_NATIVE_LDOUBLE_COMPLEX,
H5T_CPLX_IEEE_F16LE, H5T_CPLX_IEEE_F16BE,
H5T_CPLX_IEEE_F32LE, H5T_CPLX_IEEE_F32BE,
H5T_CPLX_IEEE_F64LE and H5T_CPLX_IEEE_F64BE

Adds hard and soft datatype conversion paths between complex number
datatypes and all the integer and floating-point datatypes, as well as
between other complex number datatypes

Adds a special conversion path between complex number datatypes and
array or compound datatypes where the in-memory layout of data is the
same between the datatypes and data can be converted directly

Adds support for complex number datatypes to the h5dump, h5ls and
h5diff/ph5diff tools. Allows h5dump '-m' option to change floating-point
printing format for float complex and double complex datatypes, as well
as long double complex if it has the same size as double complex

Adds minimal support to the h5watch and h5import tools

Adds support for the predefined complex number datatypes and
H5Tcomplex_create function to the Java wrappers. Also adds initial,
untested support to the JNI for future use with HDFView

Adds support for just the H5T_COMPLEX datatype class to the Fortran
wrappers

Adds support for the predefined complex number datatypes and
H5Tcomplex_create function to the high level library H5LT interface
for use with the H5LTtext_to_dtype and H5LTdtype_to_text functions

Changes some usages of "complex" in the library since it conflicts with
the "complex" keyword from the complex.h header. Also changes various
usages of the 

tools/lib/h5tools_type.c
@@ -74,6 +74,15 @@ h5tools_get_little_endian_type(hid_t tid)
                 p_type = H5Tcopy(H5T_STD_B64LE);
             break;
 
+        case H5T_COMPLEX:
+            if (size == 4)
+                p_type = H5Tcopy(H5T_CPLX_IEEE_F16LE);
+            else if (size == 8)
+                p_type = H5Tcopy(H5T_CPLX_IEEE_F32LE);
+            else if (size == 16)
+                p_type = H5Tcopy(H5T_CPLX_IEEE_F64LE);
+            break;
+
         case H5T_TIME:
         case H5T_OPAQUE:
         case H5T_STRING:
@@ -153,6 +162,15 @@ h5tools_get_big_endian_type(hid_t tid)
                 p_type = H5Tcopy(H5T_STD_B32BE);
             else if (size == 8)
                 p_type = H5Tcopy(H5T_STD_B64BE);
+            break;
+
+        case H5T_COMPLEX:
+            if (size == 4)
+                p_type = H5Tcopy(H5T_CPLX_IEEE_F16BE);
+            else if (size == 8)
+                p_type = H5Tcopy(H5T_CPLX_IEEE_F32BE);
+            else if (size == 16)
+                p_type = H5Tcopy(H5T_CPLX_IEEE_F64BE);
             break;
 
         case H5T_TIME:

jrmainzer/hdf5_lifeboat

tools/lib/h5tools_type.c
@@ -74,6 +74,15 @@ h5tools_get_little_endian_type(hid_t tid)
                 p_type = H5Tcopy(H5T_STD_B64LE);
             break;
 
+        case H5T_COMPLEX:
+            if (size == 4)
+                p_type = H5Tcopy(H5T_COMPLEX_IEEE_F16LE);
+            else if (size == 8)
+                p_type = H5Tcopy(H5T_COMPLEX_IEEE_F32LE);
+            else if (size == 16)
+                p_type = H5Tcopy(H5T_COMPLEX_IEEE_F64LE);
+            break;
+
         case H5T_TIME:
         case H5T_OPAQUE:
         case H5T_STRING:
@@ -153,6 +162,15 @@ h5tools_get_big_endian_type(hid_t tid)
                 p_type = H5Tcopy(H5T_STD_B32BE);
             else if (size == 8)
                 p_type = H5Tcopy(H5T_STD_B64BE);
+            break;
+
+        case H5T_COMPLEX:
+            if (size == 4)
+                p_type = H5Tcopy(H5T_COMPLEX_IEEE_F16BE);
+            else if (size == 8)
+                p_type = H5Tcopy(H5T_COMPLEX_IEEE_F32BE);
+            else if (size == 16)
+                p_type = H5Tcopy(H5T_COMPLEX_IEEE_F64BE);
             break;
 
         case H5T_TIME: